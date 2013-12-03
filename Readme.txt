6656133ff15338af279e521a0f36a0f286920b41
niaj <niaj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-03 13:21:06 +0000

    changed details in readme file
    git-svn-id: https://plugins.svn.wordpress.org/wp-custom-login-register-page/trunk@814323 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,21 @@
+=== wp Custom Login ===
+Contributors: Niaj Morshed
+Tags: login, admin, image, logo
+Requires at least: 2.7
+Tested up to: 3.7
+Stable tag: 1.1
+
+This plugin can changes the logo & background image of the login form.
+
+== Description ==
+Just Install the plugin, then you can see the login form has changed,
+after this go to the  Settings->custom-login & put your logo & background image link which you want.
+Enjoy.
+
+== Installation ==
+
+1. Upload the folder "wp custom login" to the "/wp-content/plugins/" directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+1. In Action
---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+/*
+Plugin Name: wp custom login
+Plugin URI: http://niaj.me
+Description: This is my first plugin to learn development of plugin.
+Author: Niaj Morshed
+Author URI: http://niaj.me
+Version: 1.0
+licence: GNU General Public License (GPL) version 2.
+*/
+
+function ncustom_login_option_page()
+{
+	?>
+
+	<div class="wrap">
+	<?php screen_icon(); ?>
+	<h2>Custom Login Option Page</h2>
+	<p>Welcome to the custom login plugin. Here you can edit the background &amp; your Logo for your custom login page.</p>
+	<form action="options.php" method="post" id="ncustom-login-options-form">
+	<?php settings_fields('ncustom_login_options'); ?>
+		<p>
+			<b>Link of admin login logo image</b><br />
+			<input type="text" id="n_logo_link" name="n_logo_link" value="<?php echo esc_attr( get_option('n_logo_link') ); ?>" size="80" />
+		</p>
+		<p>
+			<b>Link of admin login background image</b><br />
+			<input type="text" id="n_background_link" name="n_background_link" value="<?php echo esc_attr( get_option('n_background_link') ); ?>" size="80" />
+		</p>
+		<p>
+			<input type="submit" value="Save images Link">
+		</p>
+	</form>
+	</div>
+	<?php
+}
+
+function ncustom_login_init(){
+	register_setting('ncustom_login_options','n_logo_link');
+	register_setting('ncustom_login_options','n_background_link');
+}
+
+add_action('admin_init','ncustom_login_init');
+
+function ncustom_login_menu()
+{
+	add_options_page('Custom Login Settings','Custom Login','manage_options','custom-login','ncustom_login_option_page');
+}
+add_action('admin_menu','ncustom_login_menu');
+
+/*---------------------------*/
+
+function my_login_logo_url() {
+    return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return '';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+function my_login_stylesheet() { ?>
+    <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_bloginfo('url') . '/wp-content/plugins/wp-custom-login/style-login.css'; ?>" type="text/css" media="all" />
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+
+
+/*------------ Get custom Images ----------------*/
+function n_login_css() {
+	$n_logo_link = esc_attr( get_option('n_logo_link') );
+	$n_background_link = esc_attr( get_option('n_background_link') );
+	if($n_logo_link != '')
+	{
+		echo '<style type="text/css">
+		#login h1 a { background: url('.$n_logo_link.') no-repeat;}
+		</style>';
+	}else{
+		echo '<style type="text/css">
+		#login h1 a { background: url('.get_bloginfo("url").'/wp-content/plugins/wp-custom-login/images/logo.png) no-repeat; }
+		</style>';
+	}
+
+	if($n_background_link != '')
+	{
+		echo '<style type="text/css">
+		html { background: url('.$n_background_link.') no-repeat 50% 50% fixed;
+		background-size: 100% auto;
+		}
+		</style>';
+	}else{
+		echo '<style type="text/css">
+		html { background: url('.get_bloginfo("url").'/wp-content/plugins/wp-custom-login/images/bg1.jpg) no-repeat 50% 50% fixed;
+		background-size: 100% auto;
+		}
+		</style>';
+	}
+}
+
+add_action('login_enqueue_scripts', 'n_login_css');
